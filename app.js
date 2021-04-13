c8c0b808a2a7795ecd9faa8b7bad66f910beccf7
jacobrees <jacob.rees97@yahoo.com>
2021-04-13 17:52:48 +0100

    Refactor code for toggling menu into a function

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const player1Btn = document.querySelector('.player1-btn');
 const player2Btn = document.querySelector('.player2-btn');
 const menu = document.querySelector('.menu');
 
-player1Btn.addEventListener('click', () => {
+function toggleMenu() {
   menu.classList.toggle('hide-menu');
+}
+
+player1Btn.addEventListener('click', () => {
+  toggleMenu();
 });
 
 player2Btn.addEventListener('click', () => {
-  menu.classList.toggle('hide-menu');
+  toggleMenu();
 });
