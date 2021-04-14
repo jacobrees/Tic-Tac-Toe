b2da57dab02697e0f8fc08d263c32d792f0bdc1d
jacobrees <jacob.rees97@yahoo.com>
2021-04-14 15:34:39 +0100

    Complete functionality for when player adds move, img fades in, refactor code

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ player2Btn.addEventListener('click', () => {
   toggleMenu();
 });
 
-const squares = document.querySelectorAll('.square');
+
+const playerTurn = document.querySelector('.player-turn');
 
 let currentPlayer = 'X';
 
@@ -24,21 +25,35 @@ const switchPlayer = () => {
   } else {
     currentPlayer = 'X';
   }
-};
 
-const playerTurn = document.querySelector('.player-turn');
+  playerTurn.textContent = `Player ${currentPlayer} Turn`;
+};
 
 
 const insertSymbol = (symbol) => `<img src="img/${symbol}.svg" alt="${symbol}"></img>`;
 
+
+const fadeImg = (e) => {
+  const img = e.currentTarget.firstChild;
+  window.getComputedStyle(img).opacity; // eslint-disable-line
+  img.style.opacity = 1;
+};
+
+const addImg = (e) => {
+  e.currentTarget.innerHTML = insertSymbol(currentPlayer);
+  fadeImg(e);
+};
+
 const playerMove = (square, index) => {
   square.addEventListener('click', (e) => {
-    e.currentTarget.innerHTML = insertSymbol(currentPlayer);
+    addImg(e);
     switchPlayer();
-    playerTurn.textContent = `Player ${currentPlayer} Turn`;
-    console.log(index)
+
+    console.log(index);
   }, { once: true });
 };
+
+const squares = document.querySelectorAll('.square');
 
 const initialize = () => {
   squares.forEach((square, index) => {
