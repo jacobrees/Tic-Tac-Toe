5dc1ab02e634087c9f096dfa57265c688e90a303
jacobrees <jacob.rees97@yahoo.com>
2021-04-15 07:34:31 +0100

    Create an array of all possible winning combinations

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ const addImg = (e) => {
   fadeImg(e);
 };
 
+const winningPossibilities = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8],
+];
+
 const playerMove = (square, index) => {
   square.addEventListener('click', (e) => {
     addImg(e);
